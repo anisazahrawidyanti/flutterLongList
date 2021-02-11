cb6fbf870f6bf9abb58475f0a63051b9d31b7df2
anisazahrawidyanti <55775447+anisazahrawidyanti@users.noreply.github.com>
2021-02-11 22:52:02 +0700

    flutterLongList

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: 'Flutter Widget',
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Long List"),
+      ),
+      body: getListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint("Working");
+        },
+        child: Icon(Icons.add),
+        tooltip: "Add One More Item",
+      ),
+    ),
+  ));
+}
+
+void showSnackBar(BuildContext context, String item) {
+  var snackBar = SnackBar(content: Text("You just tapped $item"));
+
+  Scaffold.of(context).showSnackBar(snackBar);
+}
+
+List<String> getListElements() {
+  var items = List<String>.generate(1000, (counter) => "Item $counter");
+  return items;
+}
+
+Widget getListView() {
+  var listItems = getListElements();
+
+  var listView = ListView.builder(itemBuilder: (context, index) {
+    return ListTile(
+      leading: Icon(Icons.arrow_right),
+      title: Text(listItems[index]),
+      onTap: () {
+        showSnackBar(context, listItems[index]);
+      },
+    );
+  });
+
+  return listView;
+}
